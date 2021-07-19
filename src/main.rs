eb05468cdc81150f1fa8005792a26074999881ca
pyxiis <47072520+pyxiis@users.noreply.github.com>
2021-07-19 12:06:51 -0700

    Edit error for when no file is found

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,20 @@ use crate::lang::matrix::chars;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("\u{001b}[31m Exactly 1 argument is required: `filename`\u{001b}[0m");
+        println!("\u{001b}[31mExactly 1 argument is required: `filename`\u{001b}[0m");
         process::exit(1);
     }
 
     let filename: &String = &args[1];
 
-    let code: String = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let content = fs::read_to_string(filename);
 
-    let lines: Vec<Vec<char>> = chars(&code);
+    if content.is_err() {
+        println!("\u{001b}[31mNo file exists at `{}`\u{001b}[0m", filename);
+        process::exit(1);
+    }
+
+    let lines: Vec<Vec<char>> = chars(&content.unwrap());
 
     for line in lines {
         for chr in line {
