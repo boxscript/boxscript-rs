195e3830a52ce575257703a35e0a981a0d54bc7e
pyxiis <47072520+pyxiis@users.noreply.github.com>
2021-07-19 14:06:56 -0700

    Add tokens

--- src/lang/interpreter.rs
+++ src/lang/interpreter.rs
@@ -3,3 +3,26 @@ use std::collections::HashMap;
 pub trait Runnable {
     fn run(&self, memory: HashMap<i128, i128>) -> i128;
 }
+
+pub enum Token {
+    GT,
+    LT,
+    EQ,
+    NE,
+    ASSIGN,
+    NOT,
+    AND,
+    OR,
+    XOR,
+    LSHIFT,
+    RSHIFT,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    POW,
+    LPAREN,
+    RPAREN,
+    OUTPUT,
+}
