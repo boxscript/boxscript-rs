fcb2c84e1eb0c7d60f71fcd3303d4c67afdb45f8
pyxiis <47072520+pyxiis@users.noreply.github.com>
2021-07-22 09:29:12 -0700

    Remove all functionality from `main.rs`

---
+++ src/cli/env.rs
@@ -0,0 +1,66 @@
+use std::fs;
+
+fn read(args: Vec<String>) -> Result<String, String> {
+    if args.len() != 1 {
+        return Err(String::from(
+            "\u{001b}[31mExactly 1 argument is required: `filename`\u{001b}[0m",
+        ));
+    }
+
+    let filename: &String = &args[0];
+
+    let content = fs::read_to_string(filename);
+
+    if content.is_err() {
+        return Err(format!(
+            "\u{001b}[31mNo file exists at `{}`\u{001b}[0m",
+            filename
+        ));
+    }
+
+    Ok(content.unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_read() {
+        use super::read;
+
+        assert_eq!(
+            read(
+                ["a", "b"]
+                    .iter()
+                    .map(|s| String::from(*s))
+                    .collect()
+            ),
+            Err(String::from(
+                "\u{001b}[31mExactly 1 argument is required: `filename`\u{001b}[0m",
+            ))
+        );
+
+        assert_eq!(
+            read(
+                [""]
+                    .iter()
+                    .map(|s| String::from(*s))
+                    .collect()
+            ),
+            Err(String::from(
+                "\u{001b}[31mNo file exists at ``\u{001b}[0m",
+            ))
+        );
+
+        assert_eq!(
+            read(
+                ["src/cli/mod.rs"]
+                    .iter()
+                    .map(|s| String::from(*s))
+                    .collect()
+            ),
+            Ok(String::from(
+                "pub mod env;\n",
+            ))
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
 #![allow(dead_code)]
 
-use std::env;
-use std::fs;
-use std::process;
-
+mod cli;
 mod lang;
 
-use crate::lang::matrix::chars;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("\u{001b}[31mExactly 1 argument is required: `filename`\u{001b}[0m");
-        process::exit(1);
-    }
-
-    let filename: &String = &args[1];
-
-    let content = fs::read_to_string(filename);
-
-    if content.is_err() {
-        println!("\u{001b}[31mNo file exists at `{}`\u{001b}[0m", filename);
-        process::exit(1);
-    }
-
-    let lines: Vec<Vec<char>> = chars(&content.unwrap());
-
-    for line in lines {
-        for chr in line {
-            print!("{}", chr);
-        }
-        println!();
-    }
-}
+fn main() {}
