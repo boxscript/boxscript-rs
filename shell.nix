435318c283a0db068af4bb4a12c521dd70b4f97b
pyxiis <47072520+pyxiis@users.noreply.github.com>
2021-07-22 10:08:24 -0700

    Strip unnecessary components from Nix shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ rustup rustc cargo clang llvm_12 ];
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  buildInputs = with pkgs; [ rustup llvm_12 ];
 }
